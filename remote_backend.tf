dcb2ede1eb43c14141598b472b71802e41b8ac1f
Daniel Wood <mrdkwood@gmail.com>
2023-03-01 22:36:27 +0000

    Added vpc module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "mrdkwood-org"
     workspaces {
       name = "hashicat-gcp"
